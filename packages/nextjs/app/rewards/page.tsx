918f53bc432c40a3fef2830c3115d96594fc96ac
Heathcliff <kou@JT-no-MacBook-Pro.local>
2024-08-09 18:01:39 +0800

    usdc approve

--- packages/nextjs/app/rewards/page.tsx
+++ packages/nextjs/app/rewards/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useScaffoldWriteContract } from "../../hooks/scaffold-eth";
 import { NextPage } from "next";
 
 const Rewards: NextPage = () => {
@@ -7,7 +8,23 @@ const Rewards: NextPage = () => {
   //   abi:
   // })
 
-  return <div className="flex items-center justify-center h-screen">Rewards</div>;
+  const usdc = useScaffoldWriteContract("USDC");
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <button
+        onClick={async () => {
+          await usdc.writeContractAsync({
+            functionName: "approve",
+            args: ["0xB3e3757AfaB8eB52021115150caBFD6B0c838293", BigInt("10000000")],
+          });
+        }}
+      >
+        {" "}
+        approve
+      </button>
+    </div>
+  );
 };
 
 export default Rewards;
